75f0dbf5c78371bb019d11cc1a6e65b209c4c9d0
Marc Antoine HIEN <marc-antoine.hien@polymtl.ca>
2026-07-27 23:07:30 -0400

    [INFRA]: Add GitHub Actions IAM role and OIDC provider configuration

---
+++ terraform/github.tf
@@ -0,0 +1,35 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_repo}:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "${var.github_repo}-github-actions"
+  assume_role_policy = data.aws_iam_policy_document.trust.json
+}
