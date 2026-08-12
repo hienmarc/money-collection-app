7d0c244d798b481536f48ff70d2717c4a8bc6aee
Marc Antoine Hien <146673127+hienmarc@users.noreply.github.com>
2026-08-11 22:34:25 -0400

    [INFRA] Prevent prod database destroy (#14)

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -1,4 +1,17 @@
-resource "supabase_project" "money_collection_app_project" {
+resource "supabase_project" "money_collection_app_project_protected" {
+  count             = var.prevent_db_destroy ? 1 : 0
+  organization_id   = var.organization_id
+  name              = var.project_name
+  database_password = var.database_password
+  region            = var.region
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "supabase_project" "money_collection_app_project_unprotected" {
+  count             = var.prevent_db_destroy ? 0 : 1
   organization_id   = var.organization_id
   name              = var.project_name
   database_password = var.database_password
--- terraform/modules/database/variables.tf
+++ terraform/modules/database/variables.tf
@@ -1,5 +1,6 @@
 variable "organization_id" { type = string }
 variable "project_name" { type = string }
+variable "prevent_db_destroy" { type = bool }
 variable "database_password" {
   type      = string
   sensitive = true
