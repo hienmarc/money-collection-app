9359ba7503e1c10ea1a68731a2ec9d71ceea0547
Marc Antoine HIEN <marcantoinehien123@gmail.com>
2026-08-11 22:42:14 -0400

    [PATCH] Fix project_id output

--- terraform/modules/database/outputs.tf
+++ terraform/modules/database/outputs.tf
@@ -1,4 +1,9 @@
-output "project_id" { value = supabase_project.money_collection_app_project.id }
+output "project_id" {
+  value = coalesce(
+    supabase_project.money_collection_app_project_protected[0].id,
+    supabase_project.money_collection_app_project_unprotected[0].id
+  )
+}
 output "anon_key" {
   value     = data.supabase_apikeys.money_collection_app.anon_key
   sensitive = true
